1be0e36e304c3c9cb610e6c6958f2c0535b897d9
alessio-palumbo <alessio.palumbo4@gmail.com>
2025-08-02 15:22:16 +1000

    [matrix] Add more matrix manipulation methods

--- pkg/matrix/matrix.go
+++ pkg/matrix/matrix.go
@@ -4,6 +4,15 @@ import (
 	"github.com/alessio-palumbo/lifxprotocol-go/gen/protocol/packets"
 )
 
+// alignment defines the alignment of a given shape in the matrix.
+type alignment int
+
+const (
+	AlignLeft alignment = iota
+	AlignCenter
+	AlignRight
+)
+
 // Matrix represents a LIFX device with matrix capability.
 type Matrix struct {
 	Width       int
@@ -53,26 +62,104 @@ func (m *Matrix) SetPixel(x, y int, c packets.LightHsbk) {
 // wrapping to the next row.
 func (m *Matrix) SetColors(x, y int, cs ...packets.LightHsbk) {
 	for _, c := range cs {
-		if x >= m.Width && y >= m.MaxY() {
-			break
-		}
-		if x < m.Width {
-			m.SetPixel(x, y, c)
-			x++
-		} else {
+		if x >= m.Width {
+			if y >= m.MaxY() {
+				break
+			}
+			// Wrap to the beginning of the next line.
 			x = 0
 			y++
-			m.SetPixel(x, y, c)
 		}
+		m.SetPixel(x, y, c)
+		x++
 	}
 }
 
-// SetCorners sets the corner
-func (m *Matrix) SetCorners(cs [4]packets.LightHsbk) {
+// SetCorners sets the corners of a matrix to the given palette.
+// It accepts up to 4 colors and if fewer are provided it rotates through them.
+func (m *Matrix) SetCorners(palette ...packets.LightHsbk) {
+	cs := NewColorSlice(4, palette...)
 	m.SetPixel(0, 0, cs[0])
 	m.SetPixel(m.MaxX(), 0, cs[1])
 	m.SetPixel(0, m.MaxY(), cs[2])
 	m.SetPixel(m.MaxX(), m.MaxY(), cs[3])
+}
+
+// SetHorizontalSegment sets a section of row y to the given palette  starting at column x.
+func (m *Matrix) SetHorizontalSegment(x, y, length int, palette ...packets.LightHsbk) {
+	length = min(length, m.Width-x)
+	m.SetColors(x, y, NewColorSlice(length, palette...)...)
+}
+
+// SetVerticalSegment sets a section of a column x to the given palette starting at row y.
+func (m *Matrix) SetVerticalSegment(x, y, length int, palette ...packets.LightHsbk) {
+	length = min(length, m.Height-y)
+	cs := NewColorSlice(length, palette...)
+	for i := range length {
+		m.SetPixel(x, y+i, cs[i])
+	}
+}
+
+// SetXForAlignment sets x according to the given alignment.
+func (m *Matrix) SetXForAlignment(a alignment, length int) int {
+	var x int
+	if length < m.Width {
+		switch a {
+		case AlignCenter:
+			x = (m.Width - length) / 2
+		case AlignRight:
+			x = m.Width - length
+		}
+	}
+	return x
+}
+
+// NewColorSlice returns a []packets.LightHsbk of the specific length according to the palette.
+// It the palette is shorter than the length then it rotates through it to fill up the slice.
+func NewColorSlice(length int, palette ...packets.LightHsbk) []packets.LightHsbk {
+	cs := make([]packets.LightHsbk, length)
+	if len(palette) > length {
+		copy(cs, palette[:length])
+		return cs
+	}
+
+	// Rotate through the colors to fill the slice.
+	for i := range length {
+		cs[i] = palette[i%len(palette)]
+	}
+	return cs
+}
+
+// CopyRow applies the colors of a src row to the dst row.
+// If dstY or srcY are invalid it does nothing.
+func (m *Matrix) CopyRow(dstY, srcY int) {
+	if srcY > m.MaxY() || dstY > m.MaxY() {
+		return
+	}
+	copy(m.Colors[dstY], m.Colors[srcY])
+}
+
+func (m *Matrix) DrawSquare(x, y, length int, palette ...packets.LightHsbk) {
+	m.SetHorizontalSegment(x, y, length, palette...)
+	for i := range length - 1 {
+		m.CopyRow(y+1+i, y)
+	}
+}
+
+func (m *Matrix) SetBorder(padding int, colors ...packets.LightHsbk) {
+	paddedY := min(padding, m.MaxX()/2, m.MaxY()/2)
+	length := m.Width - (padding * 2)
+	height := m.Height - (padding * 2)
+	x := m.SetXForAlignment(AlignCenter, length)
+
+	// Set top
+	m.SetHorizontalSegment(x, paddedY, length, colors...)
+	// Set bottom
+	m.SetHorizontalSegment(x, m.MaxY()-paddedY, length, colors...)
+	// Set left
+	m.SetVerticalSegment(x, paddedY, height, colors...)
+	// Set right
+	m.SetVerticalSegment(m.MaxX()-x, paddedY, height, colors...)
 }
 
 // MaxX is the last valid row index.
---
+++ pkg/matrix/matrix_test.go
@@ -0,0 +1,575 @@
+package matrix
+
+import (
+	"testing"
+
+	"github.com/alessio-palumbo/lifxprotocol-go/gen/protocol/packets"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestClear(t *testing.T) {
+	clearMatrix := New(4, 4, 0).Colors
+	testCases := map[string]struct {
+		setColors func(*Matrix)
+		pixels    []Pixel
+		want      [][]packets.LightHsbk
+	}{
+		"clear all": {
+			setColors: func(m *Matrix) {
+				m.SetColors(0, 0, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600})
+			},
+			want: clearMatrix,
+		},
+		"clear by pixels": {
+			setColors: func(m *Matrix) {
+				m.SetColors(0, 0, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600})
+			},
+			pixels: []Pixel{{0, 0}, {1, 0}, {2, 0}},
+			want:   clearMatrix,
+		},
+		"clear some pixels": {
+			setColors: func(m *Matrix) {
+				m.SetColors(0, 0, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600})
+			},
+			pixels: []Pixel{{0, 0}},
+			want: [][]packets.LightHsbk{
+				{{}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			tc.setColors(m)
+			m.Clear(tc.pixels...)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetColor(t *testing.T) {
+	testCases := map[string]struct {
+		setColors func(*Matrix)
+		want      [][]packets.LightHsbk
+	}{
+		"colors shorter than width": {
+			setColors: func(m *Matrix) {
+				m.SetColors(0, 0, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600})
+			},
+			want: [][]packets.LightHsbk{
+				{packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"wraps around when colors is greater than width": {
+			setColors: func(m *Matrix) {
+				m.SetColors(
+					0, 0,
+					packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600},
+					packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600},
+				)
+			},
+			want: [][]packets.LightHsbk{
+				{packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600}, packets.LightHsbk{Kelvin: 3500}},
+				{packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"does not exceed matrix size": {
+			setColors: func(m *Matrix) {
+				m.SetColors(2, 3, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}, packets.LightHsbk{Kelvin: 3600})
+			},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, packets.LightHsbk{Kelvin: 3500}, packets.LightHsbk{Kelvin: 3700}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			tc.setColors(m)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetCorners(t *testing.T) {
+	testCases := map[string]struct {
+		palette []packets.LightHsbk
+		want    [][]packets.LightHsbk
+	}{
+		"set corners with same color": {
+			palette: []packets.LightHsbk{{Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{packets.LightHsbk{Kelvin: 3500}, {}, {}, packets.LightHsbk{Kelvin: 3500}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{packets.LightHsbk{Kelvin: 3500}, {}, {}, packets.LightHsbk{Kelvin: 3500}},
+			},
+		},
+		"set corners with fewer than 4 colors": {
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3600}},
+			want: [][]packets.LightHsbk{
+				{packets.LightHsbk{Kelvin: 3500}, {}, {}, packets.LightHsbk{Kelvin: 3600}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{packets.LightHsbk{Kelvin: 3500}, {}, {}, packets.LightHsbk{Kelvin: 3600}},
+			},
+		},
+		"set corners with greater than 4 colors": {
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3600}, {Kelvin: 3700}, {Kelvin: 3800}, {Kelvin: 3900}},
+			want: [][]packets.LightHsbk{
+				{packets.LightHsbk{Kelvin: 3500}, {}, {}, packets.LightHsbk{Kelvin: 3600}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{packets.LightHsbk{Kelvin: 3700}, {}, {}, packets.LightHsbk{Kelvin: 3800}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			m.SetCorners(tc.palette...)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetHorizontalSegment(t *testing.T) {
+	testCases := map[string]struct {
+		alignment alignment
+		row       int
+		length    int
+		palette   []packets.LightHsbk
+		want      [][]packets.LightHsbk
+	}{
+		"align left": {
+			alignment: AlignLeft,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"align right": {
+			alignment: AlignRight,
+			row:       1,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {Kelvin: 3500}, {Kelvin: 3500}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"align center": {
+			alignment: AlignCenter,
+			row:       3,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+			},
+		},
+		"palette greater than length": {
+			alignment: AlignLeft,
+			length:    3,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"palette shorter than length": {
+			alignment: AlignLeft,
+			length:    3,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3700}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3700}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			x := m.SetXForAlignment(tc.alignment, tc.length)
+			m.SetHorizontalSegment(x, tc.row, tc.length, tc.palette...)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetVerticalSegment(t *testing.T) {
+	testCases := map[string]struct {
+		col     int
+		row     int
+		length  int
+		palette []packets.LightHsbk
+		want    [][]packets.LightHsbk
+	}{
+		"offset col": {
+			col:     2,
+			length:  2,
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {Kelvin: 3500}, {}},
+				{{}, {}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"offset row": {
+			row:     2,
+			length:  2,
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{Kelvin: 3500}, {}, {}, {}},
+			},
+		},
+		"palette greater than length": {
+			length:  3,
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"palette shorter than length": {
+			length:  3,
+			palette: []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3700}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{Kelvin: 3700}, {}, {}, {}},
+				{{Kelvin: 3500}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			m.SetVerticalSegment(tc.col, tc.row, tc.length, tc.palette...)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetXForAlignment(t *testing.T) {
+	testCases := map[string]struct {
+		matrix    *Matrix
+		alignment alignment
+		length    int
+		want      int
+	}{
+		"align left": {
+			matrix:    New(4, 4, 0),
+			alignment: AlignLeft,
+			length:    2,
+			want:      0,
+		},
+		"align center": {
+			matrix:    New(4, 4, 0),
+			alignment: AlignCenter,
+			length:    2,
+			want:      1,
+		},
+		"align right": {
+			matrix:    New(4, 4, 0),
+			alignment: AlignRight,
+			length:    2,
+			want:      2,
+		},
+		"align center: odd widht": {
+			matrix:    New(7, 5, 0),
+			alignment: AlignCenter,
+			length:    5,
+			want:      1,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := tc.matrix.SetXForAlignment(tc.alignment, tc.length)
+			assert.Equal(t, got, tc.want)
+		})
+	}
+}
+
+func TestNewColorsSlice(t *testing.T) {
+	testCases := map[string]struct {
+		length  int
+		palette []packets.LightHsbk
+		want    []packets.LightHsbk
+	}{
+		"palette greater than length": {
+			length: 4,
+			palette: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500},
+				{Kelvin: 3500}, {Kelvin: 3500},
+			},
+			want: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3500},
+				{Kelvin: 3500}, {Kelvin: 3500},
+			},
+		},
+		"palette equal length": {
+			length: 4,
+			palette: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3500},
+				{Kelvin: 3500}, {Kelvin: 3500},
+			},
+			want: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3500},
+				{Kelvin: 3500}, {Kelvin: 3500},
+			},
+		},
+		"palette shorter than length": {
+			length: 4,
+			palette: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3700},
+			},
+			want: []packets.LightHsbk{
+				{Kelvin: 3500}, {Kelvin: 3700},
+				{Kelvin: 3500}, {Kelvin: 3700},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := NewColorSlice(tc.length, tc.palette...)
+			assert.Equal(t, got, tc.want)
+		})
+	}
+}
+
+func TestCopyRow(t *testing.T) {
+	testCases := map[string]struct {
+		dstY, srcY int
+		want       [][]packets.LightHsbk
+	}{
+		"copies previous row": {
+			srcY: 0,
+			dstY: 1,
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"copies from bottom row": {
+			srcY: 3,
+			dstY: 0,
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"skips copy with invalid srcY": {
+			srcY: 5,
+			dstY: 1,
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"skips copy with invalid dstY": {
+			srcY: 0,
+			dstY: 5,
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			m.Colors = [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+			}
+			m.CopyRow(tc.dstY, tc.srcY)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+func TestDrawSquare(t *testing.T) {
+	testCases := map[string]struct {
+		alignment alignment
+		row       int
+		length    int
+		palette   []packets.LightHsbk
+		want      [][]packets.LightHsbk
+	}{
+		"align left": {
+			alignment: AlignLeft,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"align right": {
+			alignment: AlignRight,
+			row:       1,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {Kelvin: 3500}, {Kelvin: 3500}},
+				{{}, {}, {Kelvin: 3500}, {Kelvin: 3500}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"align center": {
+			alignment: AlignCenter,
+			row:       2,
+			length:    2,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+			},
+		},
+		"palette greater than length": {
+			alignment: AlignLeft,
+			length:    3,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"palette shorter than length": {
+			alignment: AlignLeft,
+			length:    3,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}, {Kelvin: 3700}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3700}, {Kelvin: 3500}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3700}, {Kelvin: 3500}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3700}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"row overflows": {
+			alignment: AlignLeft,
+			row:       2,
+			length:    3,
+			palette:   []packets.LightHsbk{{Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {}, {}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			m := New(4, 4, 0)
+			x := m.SetXForAlignment(tc.alignment, tc.length)
+			m.DrawSquare(x, tc.row, tc.length, tc.palette...)
+			assert.Equal(t, m.Colors, tc.want)
+		})
+	}
+}
+
+func TestSetBorder(t *testing.T) {
+	testCases := map[string]struct {
+		matrix  *Matrix
+		padding int
+		palette []packets.LightHsbk
+		want    [][]packets.LightHsbk
+	}{
+		"outer border": {
+			matrix:  New(4, 4, 0),
+			palette: []packets.LightHsbk{{Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}},
+				{{Kelvin: 3500}, {}, {}, {Kelvin: 3500}},
+				{{Kelvin: 3500}, {}, {}, {Kelvin: 3500}},
+				{{Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}},
+			},
+		},
+		"with padding": {
+			matrix:  New(4, 4, 0),
+			padding: 1,
+			palette: []packets.LightHsbk{{Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}},
+			},
+		},
+		"pads irregular matrix": {
+			matrix:  New(7, 5, 0),
+			padding: 1,
+			palette: []packets.LightHsbk{{Kelvin: 3500}},
+			want: [][]packets.LightHsbk{
+				{{}, {}, {}, {}, {}, {}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {Kelvin: 3500}, {}, {}, {}, {Kelvin: 3500}, {}},
+				{{}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {Kelvin: 3500}, {}},
+				{{}, {}, {}, {}, {}, {}, {}},
+			},
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			tc.matrix.SetBorder(tc.padding, tc.palette...)
+			assert.Equal(t, tc.matrix.Colors, tc.want)
+		})
+	}
+}
