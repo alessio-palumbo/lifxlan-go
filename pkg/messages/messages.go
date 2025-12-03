027a2e1944750e5356e3fba0f841d8d0dc0466c8
alessio-palumbo <alessio.palumbo4@gmail.com>
2025-12-04 10:45:58 +1100

    [messages] Add message to set colors to multizone devices

--- pkg/messages/messages.go
+++ pkg/messages/messages.go
@@ -55,6 +55,7 @@ func SetColor(h, s, b *float64, k *uint16, d time.Duration, waveform enums.Light
 	return protocol.NewMessage(m)
 }
 
+// SetMatrixColors returns a TileSet64 Message that sets a matrix with the given size to the provided colors.
 func SetMatrixColors(startIndex, length, width int, colors [64]packets.LightHsbk, d time.Duration) *protocol.Message {
 	m := &packets.TileSet64{
 		TileIndex: uint8(startIndex),
@@ -64,4 +65,38 @@ func SetMatrixColors(startIndex, length, width int, colors [64]packets.LightHsbk
 		Colors:    colors,
 	}
 	return protocol.NewMessage(m)
+}
+
+const extendedMultizoneMsgMaxZones = 82
+
+// SetMultizoneExtendedColors accepts a variable length list of colors and returns multiple MultiZoneExtendedSetColorZones
+// messages to cater for devices with more than the message maximum supported zones.
+// If a single message is needed than the Apply directive is set on the message itself, otherwise an extra message
+// is produced to Apply the colors previously buffered by the device.
+// The startIndex refers to the zone the colors should apply from, if set to 0 colors will be applied from the first zone.
+func SetMultizoneExtendedColors(startIndex int, colors []packets.LightHsbk, d time.Duration) []*protocol.Message {
+	var msgs []*protocol.Message
+	nColors := len(colors)
+
+	for offset := 0; offset < nColors; offset += extendedMultizoneMsgMaxZones {
+		end := min(nColors, offset+extendedMultizoneMsgMaxZones)
+		chunk := colors[offset:end]
+
+		m := &packets.MultiZoneExtendedSetColorZones{
+			Index:       uint16(startIndex + offset),
+			ColorsCount: uint8(len(chunk)),
+			Duration:    uint32(d.Milliseconds()),
+		}
+		copy(m.Colors[:], chunk)
+		msgs = append(msgs, protocol.NewMessage(m))
+	}
+
+	if len(msgs) == 1 {
+		msgs[0].Payload.(*packets.MultiZoneExtendedSetColorZones).Apply = enums.MultiZoneExtendedApplicationRequest(enums.MultiZoneApplicationRequestMULTIZONEAPPLICATIONREQUESTAPPLY)
+	} else {
+		m := &packets.MultiZoneExtendedSetColorZones{Apply: enums.MultiZoneExtendedApplicationRequestMULTIZONEEXTENDEDAPPLICATIONREQUESTAPPLYONLY}
+		msgs = append(msgs, protocol.NewMessage(m))
+	}
+
+	return msgs
 }
